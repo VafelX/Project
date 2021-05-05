f921e7ca9a60bb970a02062607388d3f33197258
Vidix <72098622+VafelX@users.noreply.github.com>
2021-05-06 00:43:20 +0300

    qwerty

--- app/src/main/java/com/example/projectbeta/Alarm.java
+++ app/src/main/java/com/example/projectbeta/Alarm.java
@@ -4,15 +4,12 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
-import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.TextClock;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
@@ -20,19 +17,19 @@ import java.util.Calendar;
 public class Alarm extends AppCompatActivity {
 
     private AppCompatButton imgbtn_on, imgbtn_off;
-    MediaPlayer mediaPlayer;
-    AlarmManager alarmManager;
+    //AlarmManager alarmManager;
     TimePicker timePicker;
-    TextView tv_update;
     PendingIntent pendingIntent;
+    TextClock textClock;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
         timePicker = (TimePicker) findViewById(R.id.timepecker);
-        tv_update = findViewById(R.id.tv_update);
-        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        textClock = findViewById(R.id.txtClock);
+        //alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
         imgbtn_off = findViewById(R.id.btn_off);
         imgbtn_off.setOnClickListener(new View.OnClickListener() {
@@ -52,8 +49,8 @@ public class Alarm extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
-                calendar.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
-                calendar.set(Calendar.MINUTE, timePicker.getMinute());
+//                calendar.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
+//                calendar.set(Calendar.MINUTE, timePicker.getMinute());
 
                 int hour = timePicker.getHour();
                 int min = timePicker.getMinute();
@@ -61,57 +58,77 @@ public class Alarm extends AppCompatActivity {
                 String hour_string = String.valueOf(hour);
                 String min_string = String.valueOf(min);
 
-                if (hour > 12) {
-                    hour_string = String.valueOf(hour - 12);
-                }
-                if (min < 10) {
-                    min_string = "0" + String.valueOf(min);
-                }
 
-                setTimeText("Будильник поставлен на " + hour_string + ":" + min_string);
 
-                pendingIntent = PendingIntent.getBroadcast(Alarm.this, 0, my_intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+              pendingIntent = PendingIntent.getBroadcast(Alarm.this, 0, my_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+                //12-вой формат
+                Intent i = new Intent(Alarm.this, MainActivity.class);
+                String time12 = AlarmTime12();
+                String times12 = "Будильник поставлен на: " + AlarmTime12();
+                i.putExtra("times12", times12);
+                i.putExtra("time12", time12);
+                //24-вой формат
+                String time24 = AlarmTime24();
+                String times24 = "Будильник поставлен на: " + AlarmTime24();
+                i.putExtra("times24", times24);
+                i.putExtra("time24", time24);
+                startActivity(i);
 
             }
         });
 
-    }
 
-    private void setTimeText(String s) {
-        tv_update.setText(s);
 
     }
 
-    public void play(View v) {
-        if (mediaPlayer == null) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.daydream);
-            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mp) {
-                    stopPlayer();
-                }
-            });
+    public String AlarmTime12(){
+        Integer alarmHours12 = timePicker.getCurrentHour();
+        Integer alarmMinutes12 = timePicker.getCurrentMinute();
+        String stringAlMinutes12;
+
+        if (alarmMinutes12 < 10){
+            stringAlMinutes12 = "0";
+            stringAlMinutes12 = stringAlMinutes12.concat(alarmMinutes12.toString());
+        }else {
+            stringAlMinutes12 = alarmMinutes12.toString();
         }
 
-        mediaPlayer.start();
-    }
+        String alarmTime12;
 
-    public void stop(View v) {
-        stopPlayer();
-    }
-
-    public void stopPlayer() {
-        if (mediaPlayer != null) {
-            mediaPlayer.release();
-            mediaPlayer = null;
+        if (alarmHours12 > 12){
+            alarmHours12 = alarmHours12 - 12;
+            alarmTime12 = alarmHours12.toString().concat(":").concat(stringAlMinutes12).concat(" PM");
+        }else {
+            alarmTime12 = alarmHours12.toString().concat(":").concat(stringAlMinutes12).concat(" AM");
         }
+
+        return alarmTime12;
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        stopPlayer();
+
+
+    public String AlarmTime24(){
+        Integer alarmHours24 = timePicker.getCurrentHour();
+        Integer alarmMinutes24 = timePicker.getCurrentMinute();
+
+        String stringAlMinutes12;
+
+        if (alarmMinutes24 < 10){
+            stringAlMinutes12 = "0";
+            stringAlMinutes12 = stringAlMinutes12.concat(alarmMinutes24.toString());
+        }else {
+            stringAlMinutes12 = alarmMinutes24.toString();
+        }
+
+        String alarmTime24;
+
+        alarmTime24 = alarmHours24.toString().concat(":").concat(stringAlMinutes12);
+
+
+        return alarmTime24;
     }
+
 }
--- app/src/main/java/com/example/projectbeta/MainActivity.java
+++ app/src/main/java/com/example/projectbeta/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.projectbeta;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton imgbtn_plus;
+    TextView tv_time;
+    TextClock tvClock;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imgbtn_plus = (ImageButton) findViewById(R.id.imgBtn_plus);
+        tv_time = (TextView) findViewById(R.id.tv_time);
+        tvClock = findViewById(R.id.tvClock);
 
         imgbtn_plus.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +39,35 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM));
+        Intent i = getIntent();
+        String timeHM12 = i.getStringExtra("time12");
+
+        String timeHM24 = i.getStringExtra("time24");
+
+
+        Timer t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (tvClock.getText().toString().equals(timeHM12) || tvClock.getText().toString().equals(timeHM24)){
+                    r.play();
+                }else {
+                    r.stop();
+                }
+            }
+        },0,1000);
+
+
+
+        Intent intent = getIntent();
+        String times12 =  intent.getStringExtra("times12");
+        tv_time.setText(times12);
+
+
+
     }
+
+
 }
